5b75d2cdbeae56e2ace6dad1ab69ecda2a492705
ltibm90 <ltibm90@gmail.com>
2020-10-13 23:45:31 +0300

    Added support xpath for inlclude tag

--- TextEngine/Evulator/IncludeEvulator.cs
+++ TextEngine/Evulator/IncludeEvulator.cs
@@ -12,22 +12,55 @@ namespace TextEngine.Evulator
         {
             var loc = tag.GetAttribute("name");
             loc = this.EvulateText(loc)?.ToString();
-            if (!this.ConditionSuccess(tag, "if")) return null;
-            if (!File.Exists(loc)) return null;
+            if (!this.ConditionSuccess(tag, "if") || !File.Exists(loc)) return null;
+            string xpath = tag.GetAttribute("xpath");
+            bool xpathold = false;
+            if (string.IsNullOrEmpty(xpath))
+            {
+                xpath = tag.GetAttribute("xpath_old");
+                xpathold = true;
+            }
+
             var content = File.ReadAllText(loc);
             var result = new TextEvulateResult();
-            this.Evulator.Parse(tag.Parent, content);
+
             result.Result = TextEvulateResultEnum.EVULATE_NOACTION;
             tag.Parent.SubElements.Remove(tag);        
+            if(string.IsNullOrEmpty(xpath))
+            {
+                this.Evulator.Parse(tag.Parent, content);
+            }
+            else
+            {
+                var tempitem = new TextElement();
+                tempitem.ElemName = "#document";
+                this.Evulator.Parse(tempitem, content);
+                TextElements elems = null;
+                if (!xpathold)
+                {
+                    elems = tempitem.FindByXPath(xpath);
+                }
+                else
+                {
+                    elems = tempitem.FindByXPathOld(xpath);
+                }
+                for (int i = 0; i < elems.Count; i++)
+                {
+                    elems[i].Parent = tag.Parent;
+                    tag.Parent.SubElements.Add(@elems[i]);
+                }
+            }
             return result;
         }
         private TextEvulateResult Render_Default(TextElement tag, object vars)
         {
             var loc = tag.GetAttribute("name");
+            loc = this.EvulateText(loc)?.ToString();
             var parse = tag.GetAttribute("parse", "true");
-            if (!File.Exists(loc)) return null;
+            if (!File.Exists(loc) || !this.ConditionSuccess(tag, "if")) return null;
             var content = File.ReadAllText(loc);
             var result = new TextEvulateResult();
+
             if (parse == "false")
             {
                 result.Result = TextEvulateResultEnum.EVULATE_TEXT;
@@ -40,7 +73,42 @@ namespace TextEngine.Evulator
                     ElemName = "#document",
                     BaseEvulator = this.Evulator
                 };
-                this.Evulator.Parse(tempelem, content);
+                var tempelem2 = new TextElement
+                {
+                    ElemName = "#document",
+                    BaseEvulator = this.Evulator
+                };
+
+                string xpath = tag.GetAttribute("xpath");
+                bool xpathold = false;
+                if (string.IsNullOrEmpty(xpath))
+                {
+                    xpath = tag.GetAttribute("xpath_old");
+                    xpathold = true;
+                }
+      
+                this.Evulator.Parse(tempelem2, content);
+                if (string.IsNullOrEmpty(xpath))
+                {
+                    tempelem = tempelem2;
+                }
+                else
+                {
+                    TextElements elems = null;
+                    if (!xpathold)
+                    {
+                        elems = tempelem2.FindByXPath(xpath);
+                    }
+                    else
+                    {
+                        elems = tempelem2.FindByXPathOld(xpath);
+                    }
+                    for (int i = 0; i < elems.Count; i++)
+                    {
+                        elems[i].Parent = tempelem;
+                        tempelem.SubElements.Add(elems[i]);
+                    }
+                }
                 var cresult = tempelem.EvulateValue(0, 0, vars);
                 result.TextContent += cresult.TextContent;
                 if (cresult.Result == TextEvulateResultEnum.EVULATE_RETURN)
